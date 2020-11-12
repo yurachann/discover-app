e7682e2f2aa9b80ac17b2559ad3d4d335ef32e64
yurachann <motomu1220@gmail.com>
2020-11-12 20:23:24 +0900

    ルーティングの設定

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,24 @@
 class GamesController < ApplicationController
   def index
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = GameForm.new(game_params)
+    if @game.save?
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:image, :title, :explain, :price)
+  end
+
 end
